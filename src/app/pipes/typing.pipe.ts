e0e325d3691c9fb67abdbad283aa46b4851c345f
X018161 <jarh@gft.com>
2025-11-01 17:12:04 +0100

    feat: styles refactor

---
+++ src/app/pipes/typing.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { scan, take } from 'rxjs/operators';
+
+@Pipe({
+  name: 'typing',
+  standalone: true
+})
+export class TypingPipe implements PipeTransform {
+  transform(value: string, speed: number = 50): Observable<string> {
+    if (!value) return new Observable<string>(observer => observer.complete());
+    return interval(speed).pipe(
+      take(value.length),
+      scan((acc, i) => acc + value[i], '')
+    );
+  }
+}
